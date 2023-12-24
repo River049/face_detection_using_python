e76d668b098a872fd0bb5ae955824f9e870d4a29
pavan kumar <136552918+River049@users.noreply.github.com>
2023-12-24 22:39:08 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,40 @@
+import cv2
+
+# Load the pre-trained face classifier
+face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+def detect_faces(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    
+    if len(faces) == 0:
+        return img
+    
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    
+    return img
+
+# Open a video capture object
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    
+    # Call the detect_faces function
+    frame = detect_faces(frame)
+    
+    # Display the frame
+    cv2.imshow('Video Face Detection', frame)
+    
+    # Break the loop if 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the video capture object
+cap.release()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
+
+       
